4429866881ff17778bf6fc97232a274f1023adb6
Alexandr-77 <62809713+Alexandr-77@users.noreply.github.com>
2022-07-21 18:49:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -52,6 +52,40 @@ $('.slyder__box').slick({
   ]
 });
 
+/////////////////////////////////
+$('.materials__slyder').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
+
 // ===============================================================
 
 let accordioBlockClick = document.querySelectorAll('.accordion__box-top');
